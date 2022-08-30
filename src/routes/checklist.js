22e7c6b398b8d4bec44448f55725d602506fa642
Rodrigo Martins <Rodrigo.MartinsFernandes@fit-tecnologia.org.br>
2022-08-30 14:08:32 -0400

    criação das views

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const Checklist = require('../models/checklist');
 
 router.post('/', async (req, res) => {
-  let { name } = req.body;
-
+  let { name } = req.body.checklist;
+  let checklist = new Checklist({name})
   try {
-    let checklists = await Checklist.create({ name });
-    res.status(200).json(checklists);
+    await checklist.save();
+    res.redirect('/checklists');
   } catch (error) {
-    res.status(422).json(error);
+    res.status(422).render('/checklists/new', {checklists: {...checklist, error}});
   }
 })
 
@@ -19,7 +19,16 @@ router.get('/', async (req, res) => {
     res.status(200).render('checklists/index', {checklists: checklists});
     //res.status(200).json(checklist);
   } catch(error) {
-    res.status(200).render('checklists/index', {error: 'Erro ao exibir as lista'});
+    res.status(500).render('checklists/index', {error: 'Erro ao exibir as lista'});
+  }
+})
+
+router.get('/new', async (req, res) => {
+  try {
+    let checklist = new Checklist();
+    res.status(200).render('checklists/new', {checklist: checklist});
+  } catch (error) {
+    res.status(500).render('checklists/new',{errors: 'Erro ao carregar o formulário'})
   }
 })
 
