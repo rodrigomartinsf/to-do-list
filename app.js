67f49008b8a09385b2f777291aa2cccb1ec1d483
Rodrigo Martins <Rodrigo.MartinsFernandes@fit-tecnologia.org.br>
2022-08-30 09:52:40 -0400

    criação das rotas

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const path = require('path');
+const checkListRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
+require('./config/database');
 
 const app = express();
 
-app.get('/',(req, res) => {
-  res.send('<h1>Minha lista de tarefas</h1>');
-})
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/json',(req, res) => {
-  res.json({title: 'Tarefa X', done: true});
-})
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
 
+//Criando as Rotas
+app.use('/',rootRouter);
+app.use('/checklists',checkListRouter);
 
 app.listen(3000, () => {
   console.log('Servidor foi iniciado!');
